8dcdbe94ee900e54b4af5e34d65c4373af2e0cb4
Ruslan <rus.onypenko@gmail.com>
2024-12-08 21:56:46 +0200

    Fixed the POST request issue

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(cors(corsOptions));
 
 app.use(express.static('dist'));
 
+app.use(express.json());
+
 let notes = [
     {
       id: "1",
@@ -46,6 +48,37 @@ app.get('/api/notes/:id', (request, response) => {
         response.status(404).end()
     }
 })
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter(note => note.id !== id);
+
+  response.status(204).end();
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0;
+  return String(maxId + 1);
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: 'content is missing' })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note);
+  
+  response.json(note);
+})
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
